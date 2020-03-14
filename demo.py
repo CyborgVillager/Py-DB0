90e1902238018a712cdda6a2d62914ad88142cee
CyborgVillager <almawijonathan@gmail.com>
2020-03-14 06:04:28 -0500

    v1 Connect to DB
     Connect to DB: Just make sure you installed psql for this to work properly. Besides that it will connect to it, elkse
                        tells the user a connection issue. You can use the link below on changing the password if you would like
                         to. Or use the template @ line 22 to do a quick pass change
                    Link to change pass info: https://www.postgresqltutorial.com/postgresql-change-password/
                    Quick change -> ALTER ROLE username WITH PASSWORD 'secret123';
                        Template to use on cmder -> ALTER ROLE postgres WITH PASSWORD 'secret123'';

--- demo.py
+++ demo.py
@@ -1,2 +1,14 @@
 from pydb import *
-print('Successful import')
+
+# try to connect to db
+try:
+    connection = psycopg2.connect(dbname='postgres',
+                                  user='postgres',
+                                  password='pass123',
+                                  host='localhost',
+                                  port='5433')
+    print('Connection is successful!')
+# connection error to user, if an error occurs
+except ConnectionError:
+    print('Unable to connect to DB, please check line 6-9 for the issue' + '\n'
+          + 'Thank you.')
