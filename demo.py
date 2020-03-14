9aae9eb8785c0aefb4a9826ba89618120be80c75
CyborgVillager <almawijonathan@gmail.com>
2020-03-14 07:40:44 -0500

    v3 User Input
    User Input: user now can input the user data, e.g for school the 'admin' can enter the user's name, age, and address.

--- demo.py
+++ demo.py
@@ -20,29 +20,83 @@ print('Basic table creation via terminal, can just type them out as well'
 print('\n')
 # ask user table name
 table_name = input('Please type your table name: ')
-table_usernames = input('Type Usernames: ')
-table_age = input('Type Age: ')
-table_address = input('Type Address: ')
+
 
 # Cursor/ table query creation
-try:
-    cursor = connection.cursor()
-    cursor.execute('''
-    CREATE TABLE student(
-    ID SERIAL,
-    table_name TEXT,
-    table_age INT,
-    table_address TEXT);''')
+def create_table():
+    try:
+        cursor = connection.cursor()
+        cursor.execute('''
+        CREATE TABLE student(
+        ID SERIAL,
+        NAME TEXT,
+        AGE INT,
+        ADDRESS TEXT);''')
 
-    # commit changes after connecting to cursor
-    connection.commit()
-    print('Table ' + table_name + ' is commited')
-    connection.close()
-    print('Table ' + table_name + ' is closed')
-    # print successful result to connection
-    print('Connection to ' + table_name + ' is successful!')
+        # commit changes after connecting to cursor
+        connection.commit()
+        print('Table ' + table_name + ' is commited')
+        connection.close()
+        print('Table ' + table_name + ' is closed')
+        # print successful result to connection
+        print('Connection to ' + table_name + ' is successful!')
 
-# error if connecting to cursor is unable to process
-except ConnectionError:
-    print('Unable to create table,' + table_name + ' please check lines 18-28!' + '\n'
-          +' Thank you.')
+    # error if connecting to cursor is unable to process
+    except ConnectionError:
+        print(
+            'Unable to create table,' + table_name + ' please check function create_table() for more information!' + '\n'
+            + ' Thank you.')
+
+
+def insert_data():
+    try:
+        connection = psycopg2.connect(dbname='postgres',
+                                      user='postgres',
+                                      password='pass123',
+                                      host='localhost',
+                                      port='5433')
+
+        # Enter user info
+        user_name = input('What is the student name?:  ')
+
+        # user age
+        user_age = int(input('How old is the student?: '))
+
+
+        # user address
+        user_addr =  input('What is ' + user_name + ' address?: ')
+
+        print('Connection to table ' + table_name + ' is a success ' + '\n')
+        cursor = connection.cursor()
+
+        # Insert user data onto db table
+        cursor.execute('''INSERT INTO ''' + table_name + '''(NAME,AGE,ADDRESS)''' +
+                       ''' VALUES (''' + '\'' +
+                        user_name + '\',' +
+                       '\'' + str(user_age) + '\',' +
+                       '\'' + user_addr + '\'' +')'+';'+'''''')
+
+        # commit changes after connecting to cursor
+        connection.commit()
+        print('Inserted data onto ' + table_name + ' it is now commited')
+        print('Submitted info to DB, username is: ' + user_name + ' age is ' + str(user_age) + ' ' +
+              user_name + ' addr is: ' + user_addr)
+        print('\n')
+        # Close connection
+        connection.close()
+        print('Inserted data onto ' + table_name + ' is now closed')
+        # print successful result to connection
+        print('Connection to ' + table_name + ' is successful!')
+
+        # error if connecting to cursor is unable to process
+    except ConnectionError:
+        print('Unable to input data onto table,' + table_name + ' please check lines 52-62!' + '\n'
+              + ' Thank you.')
+
+
+def main():
+    create_table()
+    insert_data()
+
+
+main()
