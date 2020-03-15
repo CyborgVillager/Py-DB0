e66ae15a3a2d1963760f062d9d4f45423a36dca2
CyborgVillager <almawijonathan@gmail.com>
2020-03-15 01:53:44 -0500

    v8 Updated GUI
    Updated GUI: information can be auto-seen, can now search for user's ID, and update new users/see them on the 'users'
                        list. Similiar work to https://github.com/CyborgVillager/python_final_project which has similiar props
                        Any how its great getting a refresher. Will now work w/ react for the front end side of things.
    
                        Objectives at hand:
                        * React mini project
                        * Grand Command
                        * A.I -> requires custom server (have some big plans!!!)
                        * Couple of py stuff

--- user.py
+++ user.py
@@ -5,15 +5,10 @@ except ImportError:
     print('Unable to import tk_source, make sure you have made the source folder classified as source' + '\n' +
           'Thank you')
 
-try:
-    from search import *
-except ImportError:
-    print('Unable to import search, make sure you have made the source folder classified as source' + '\n' +
-          'Thank you')
 
 root = Tk()
 # width & height
-root.geometry('500x200')
+root.geometry('500x400')
 
 
 # Aquire information from demo.py for user_name, user's age, and the users address / state
@@ -41,12 +36,66 @@ def get_data(user_name, user_age, user_addr):
         print('The data has been inserted onto table ')
         connection.commit()
         connection.close()
+        display_all()
 
     except ConnectionError:
         print('Unable to connect to DB ')
 
 
+def search(id):
+    try:
+        connection = psycopg2.connect(dbname='postgres',
+                                      user='postgres',
+                                      password='pass123',
+                                      host='localhost',
+                                      port='5433')
+        cursor = connection.cursor()
+        print('Connection to DB is successful!')
 
+
+        # Error if connecting to cursor is unable to process
+    except ConnectionError:
+        print('Unable to input data onto table, please check lines 52-62!' + '\n'
+              + ' Thank you.')
+
+# compare conditions for specific id from the db -> subistue a
+# value from id/get the db(student) that ='s to id
+    query = '''select * from student where id=%s'''
+    #  hold the data
+    cursor.execute(query, (id))
+    row = cursor.fetchone()
+    # aquire the input from row
+    display_search(row)
+    connection.commit()
+    connection.close()
+
+# input the information for display_search once row has been aquired
+def display_search(row):
+    listbox = Listbox(frame,width=20,height=5)
+    listbox.grid(row=7,column=1)
+    listbox.insert(END,row)
+
+def display_all():
+    try:
+        connection = psycopg2.connect(dbname='postgres',
+                                      user='postgres',
+                                      password='pass123',
+                                      host='localhost',
+                                      port='5433')
+        cursor = connection.cursor()
+        print('Connection to DB is successful!')
+        query = '''select * from student '''
+        cursor.execute(query)
+        row = cursor.fetchall()
+# Allows the user to see the other users info
+        listbox = Listbox(frame, width=20, height=5)
+        listbox.grid(row=12, column=1)
+        for x in row: listbox.insert(END, x)
+
+        # Error if connecting to cursor is unable to process
+    except ConnectionError:
+        print('Unable to input data onto table, please check lines 52-62!' + '\n'
+              + ' Thank you.')
 
 # Canvas information
 canvas = Canvas(root, height=500, width=200)
@@ -87,8 +136,6 @@ button = Button(frame, text='Submit', command=lambda: get_data(entry_username.ge
 # Submit button Location
 button.grid(row=4, column=1)
 
-
-
 # Search Feature
 label = Label(frame, text='Search User ID')
 label.grid(row=5, column=1)
@@ -100,11 +147,13 @@ entry_user_search.grid(row=6, column=1)
 label = Label(frame, text='Search: ')
 label.grid(row=6, column=0)
 
+
+
+
 # Submit Search Button
-button = Button(frame, text='Submit', command=lambda:search(entry_user_search.get()))
+button = Button(frame, text='Submit', command=lambda: search(entry_user_search.get()))
 # Submit Search button Location
 button.grid(row=6, column=2)
 
-
-
+display_all()
 root.mainloop()
